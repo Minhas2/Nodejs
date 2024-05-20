6364c52d6c8f4e16098628a250aaba0ed66be529
Minhas2 <103770686+Minhas2@users.noreply.github.com>
2024-05-20 11:02:43 +0000

    data types

---
+++ .devcontainer/01-basics/variables.js
@@ -0,0 +1,15 @@
+const account_ID=1204
+let accountEmail="@something"
+var password="5421"
+accountcity="lahore"
+
+// account_ID=2
+console.log(account_ID)
+
+accountEmail="@something1"
+password="54221"
+accountcity="laho2re"
+
+// display in tabler form 
+
+console.table([account_ID,password,accountcity,accountEmail])
