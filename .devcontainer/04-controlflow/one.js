17ba02a64058b789bdc61feeaec22ba01b3fe667
Minhas2 <103770686+Minhas2@users.noreply.github.com>
2024-05-22 11:40:27 +0000

    control flow

---
+++ .devcontainer/04-controlflow/one.js
@@ -0,0 +1,53 @@
+// Code with corrections
+
+if (true) {
+    // Do something if true
+}
+
+if (false) {
+    // Do something if false
+}
+
+const isUserLoggedIn = true;
+
+if (isUserLoggedIn) {
+    // Do something if user is logged in
+}
+
+// const temp = 21;
+
+// if (temp === 30) {
+//     console.log("Eat ice cream");
+// } else {
+//     console.log("Eat dinner");
+// }
+
+
+// const score=400
+
+// if (score>100){
+//     let power="sing"
+//     console.log(`User will: ${power}`);
+
+// }
+
+
+
+// below is implict scope
+const balance=1000
+
+// if (balance>1000) console.log("test");
+
+
+
+// nested if
+
+// if (balance<500) {
+//     console.log("less then 500");
+// } else if(balance>500){
+//     console.log("greater then 500");
+// } else (balance===1000){
+//     console.log("equal to 1000");
+// }
+
+
